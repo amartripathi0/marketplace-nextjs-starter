b6cc2e8f8ae105940ec79598ae2479d14074192b
amartripathi0 <theamartripathi@gmail.com>
2024-04-05 19:43:45 +0530

    added:project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen mx-auto max-w-[100rem]">
+     
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          951: '#f4eee5',
+          952: '#222',
+          953: '#E9E4DB',
+        },
+        green: {
+          951: '#9CAE96',
+        },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
